2475babe48298b2ac07de8b6ccdac3268390ae5f
s1250125 <40895808+s1250125@users.noreply.github.com>
2019-05-21 17:50:40 +0900

    [Fix]最短経路を求める関数で、交差地点の数も引数に出来るようにした。

--- Phase1_new/Ex4/func_shortest.h
+++ Phase1_new/Ex4/func_shortest.h
@@ -8,7 +8,7 @@
 extern void generateEdge(double (*)[NMAX], point_t*,
 			 int (*)[2], int, int, int);
 extern double calcDistance(point_t, point_t);
-extern double searchShortestPath(point_t*, double (*)[NMAX], int, int, int);
+extern double searchShortestPath(point_t*, double (*)[NMAX], int, int, int,int);//last <int> is number of crossing
 
 #endif
 
