d92eae8d635ee7f6524beb210bb5b4c71f781e4f
KongWenge <galaxykamuy@gmail.com>
2019-05-22 12:02:07 +0900

    [Fix] テスト結果を正しいものに更新した

--- Phase1_new/Ex4/func_shortest.h
+++ Phase1_new/Ex4/func_shortest.h
@@ -8,9 +8,8 @@
 extern void generateEdge(double (*)[NMAX], point_t*,
 			 int (*)[2], int, int, int);
 extern double calcDistance(point_t, point_t);
-extern double searchShortestPath(point_t*, double (*)[NMAX], int, int, int);
-extern int searchPointIndex(point_t*, int, int);
 extern double searchShortestPath(point_t*, double (*)[NMAX], int, int, int, int);//last <int> is number of crossing
+extern int searchPointIndex(point_t*, int, int);
 
 #endif
 
